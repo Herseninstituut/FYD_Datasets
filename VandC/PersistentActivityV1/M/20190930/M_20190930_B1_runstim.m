669029e6303c95754f7207a04ca1711d7015f6bd
Chris van der Togt <togt@herseninstituut.knaw.nl>
2024-10-31 17:14:32 +0100

    updated links

--- VandC/PersistentActivityV1/M/20190930/M_20190930_B1_runstim.m
+++ VandC/PersistentActivityV1/M/20190930/M_20190930_B1_runstim.m
@@ -1,1 +1,1 @@
-https://fyd.nin.nl/?path=VandC/PersistentActivityV1/M/20190930/M_20190930_B1_runstim.m
+https://fyd.nin.nl/?path=/home/fyd/VS03/VS03-VandC-TEMP/VandC_DATA/PersistentActivityV1/M/20190930/M_20190930_B1_runstim.m
--- VandC/PersistentActivityV1/M/20191009/M_20191009_B1_runstim.m
+++ VandC/PersistentActivityV1/M/20191009/M_20191009_B1_runstim.m
@@ -1,1 +1,1 @@
-https://fyd.nin.nl/?path=VandC/PersistentActivityV1/M/20191009/M_20191009_B1_runstim.m
+https://fyd.nin.nl/?path=/home/fyd/VS03/VS03-VandC-TEMP/VandC_DATA/PersistentActivityV1/M/20191009/M_20191009_B1_runstim.m
